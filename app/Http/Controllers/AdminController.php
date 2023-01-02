02974429b5f38ed0d3068742a58e863168728617
rjmstor <rianjijim123@gmail.com>
2023-01-02 13:08:16 +0700

    adminController user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,8 +10,5 @@ class AdminController extends Controller
         $users = Users::where('role','mahasiswa')->get();
         return view('home.admin.index', ['users'=>$users]);
     }
-    public function users(){
-        $users = Users::where('role','mahasiswa')->get();
-        return view('home.admin.users', ['users'=>$users]);
-    }
+    
 }
