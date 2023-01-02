fd1c1d16cfd3f335392333a92f3f68a4a3768946
rjmstor <rianjijim123@gmail.com>
2023-01-02 09:50:31 +0700

    tambah user

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function user(){
+        $users = Users::where('role','mahasiswa')->get();
+        return view('home.admin.index', ['users'=>$users]);
+    }
+    public function users(){
+        $users = Users::where('role','mahasiswa')->get();
+        return view('home.admin.users', ['users'=>$users]);
+    }
+}
